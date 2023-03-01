6618cb5f637140a35f4100565fe316ca984fc09b
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-03-01 18:01:02 +0600

    Student Component

--- taskopedia/src/index.js
+++ taskopedia/src/index.js
@@ -41,6 +41,23 @@ function MainBody(){
   );
 }
 
+function Students(){
+  const fullName = "Masum Rayhan";
+  const programmimngExp = 2;
+  return(
+    <div className = "container p-4">
+      <div className="row">Student Enrolled</div>
+      <div className="row border">
+        <div className="col-2">Image</div>
+        <div className="col-10">
+          {fullName}<br/>
+          Programming Experience {programmimngExp} Years;
+        </div>
+      </div>
+    </div>
+  )
+}
+
 function Footer(){
   return <p>Happy Coading!</p>;
 }
@@ -48,6 +65,7 @@ root.render(
   <div>
     <Header/>
     <MainBody/>
+    <Students/>
     <Footer/>
   </div>
 );
