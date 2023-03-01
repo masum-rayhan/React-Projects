c37e4e9a89c8c0b06ccf10220d816ab87fbd33b1
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-03-01 17:25:22 +0600

    Inline Style

--- taskopedia/src/index.js
+++ taskopedia/src/index.js
@@ -2,9 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const mainHeaderStyle = {
+  color : "blueviolet",
+  backgroundColor : "lightgray"
+}
+function MainHeader(){
+  return <h1 style ={mainHeaderStyle}>REACT COURSE</h1>;
+}
+function SubHeader(){
+  return <p style={{color:"gray",backgroundColor:"black"}}>This will be an exciting course!</p>;
+}
+function Header(){
+  return (
+  <div>
+    <MainHeader></MainHeader>
+    <SubHeader></SubHeader>
+  </div>
+  );
+}
+
+function MainBody(){
+  return(
+    <div>
+      <p>In this course, we will learn TaskOPedia!</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to walmart</li>
+      </ul>
+    </div>
+  );
+}
+
+function Footer(){
+  return <p>Happy Coading!</p>;
+}
 root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
+  <div>
+    <Header/>
+    <MainBody/>
+    <Footer/>
+  </div>
 );
 
