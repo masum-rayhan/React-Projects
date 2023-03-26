21b495f17bbf7ad58916df5821cb6eead8d8e52f
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-03-27 01:01:13 +0600

    studdents components

--- taskopedia/src/index.js
+++ taskopedia/src/index.js
@@ -8,11 +8,11 @@ function MainHeader() {
 }
 
 const subHeaderStyle = {
-  color:"blueviolet",
-  backgroundColor:"lightgray"
-}
+  color: "blueviolet",
+  backgroundColor: "lightgray",
+};
 function SubHeader() {
-  return <p style={subHeaderStyle}>This will be an  exciting course.</p>;
+  return <p style={subHeaderStyle}>This will be an exciting course.</p>;
 }
 
 function Header() {
@@ -25,33 +25,60 @@ function Header() {
 }
 
 function MainBody() {
-
   const whatWeWillLearn = "React JS";
   const totalLecture = 3;
 
   return (
     <div>
-      <p>In this course, we will learn {whatWeWillLearn} by building TaskOpedia!</p> <br/>
+      <p>
+        In this course, we will learn {whatWeWillLearn} by building TaskOpedia!
+      </p>{" "}
+      <br />
       <p>Total Lecture - {totalLecture}</p>
       <ul>
         <li>Basic Foundation</li>
         <li>Functional & Class Components</li>
       </ul>
-      <div>
+      {/* <div>
         Enter Task : <input maxLength={5} readOnly={false} placeholder="Add New Course?"></input>
+      </div> */}
+    </div>
+  );
+}
+
+function Students() {
+  const fullName = "Masum Rayhan";
+  const programmingExp = 1.5;
+  return (
+    <div className="container p-4">
+      <div className="row">Students Enrolled</div>
+      <div className="row border">
+        <div className="col-2">
+          <img 
+          src={`https://ui-avatars.com/api/?name=${fullName}`} 
+          alt="UI-Avatars"
+          className="w-100"></img>
+        </div> 
+        <div className="col-10">
+          {fullName} <br/>
+          Programming Experiance: {programmingExp} Years.
+        </div>
       </div>
     </div>
   );
 }
 
 function Footer() {
-  return <p style={{color:"gray",backgroundColor:"black"}}>Happy Coading!</p>;
+  return (
+    <p style={{ color: "gray", backgroundColor: "black" }}>Happy Coading!</p>
+  );
 }
 
 root.render(
   <div>
     <Header />
     <MainBody />
+    <Students />
     <Footer />
   </div>
 );
