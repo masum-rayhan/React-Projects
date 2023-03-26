16ba90af02f4591cab914588148e5062cf4cac51
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-03-26 23:43:18 +0600

    first react component

--- taskopedia/src/index.js
+++ taskopedia/src/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+function Header(){
+  return (
+    <div>React Course</div>
+  )
+}
+
+function MainBody() {
+  return (
+    <div>
+      <p>In this course, we will learn react js by building TaskOpedia!</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to walcart</li>
+      </ul>
+    </div>
+  );
+}
+
+function Footer(){
+  return (
+    <div>Happy Coading!</div>
+  )
+}
+
 root.render(
-  <div>Hello World!</div>
+  <div>
+    <Header/>
+    <MainBody/>
+    <Footer/>
+  </div>
 );
