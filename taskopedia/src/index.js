7ddc8a4167b57d20095a076b57ee56471a7164d8
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-03-27 00:28:15 +0600

    custom classes

--- taskopedia/src/index.js
+++ taskopedia/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./CSS/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 function MainHeader() {
-  return <h1 className="text-primary">React Course</h1>;
+  return <h1 className="heading1">React Course</h1>;
 }
 
 const subHeaderStyle = {
