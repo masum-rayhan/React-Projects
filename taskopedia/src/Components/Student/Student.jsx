aa4c2403e7144bda25a9049004d763c0449244a4
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-03-29 19:57:04 +0600

    class components

--- taskopedia/src/Components/Student/Student.jsx
+++ taskopedia/src/Components/Student/Student.jsx
@@ -1,24 +1,28 @@
- const Students = (props) => {
+import React from "react";
+ 
+ class Students extends React.Component {
+  render() {
   return (
     <div className="col-4 p-1">
       <div className="row border">
         <div className="col-2">
           <img
-            src={props.headshot}
+            src={this.props.headshot}
             alt="UI-Avatars"
             className="w-100 py-2"
           ></img>
         </div>
         <div className="col-8">
-          {props.name} <br />
-          Programming Experiance: {props.experience} Years.
+          {this.props.name} <br />
+          Programming Experiance: {this.props.experience} Years.
         </div>
         <div className="col-2">
-          {props.children}
+          {this.props.children}
         </div>
       </div>
     </div>
   );
+  }
 }
 
 export default Students;
