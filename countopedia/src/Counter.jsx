6b757544c7d85d0a001b7fbe94eca245639bf006
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-03-31 03:22:47 +0600

    setting & retrieving state

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React from "react";
 class Counter extends React.Component {
 
-handlerAttack(){
-  alert("Attack")
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
 
-handlerDefence(){
-  alert("Defend")
-}
+  handlerAttack() {
+    alert("Attack");
+  }
+
+  handlerDefence() {
+    alert("Defend");
+  }
 
   render() {
     return (
-      <div className='row text-white'>
-        <h1>Counter: </h1>
-        <button onClick={this.handlerAttack} style={{width:"200px"}}>+1</button>
-        <button onClick={this.handlerDefence} style={{width:"200px"}}>-1</button>
+      <div className="row text-white">
+        <h1>Counter:{this.state.count} </h1>
+        <button onClick={this.handlerAttack} style={{ width: "200px" }}>
+          +1
+        </button>
+        <button onClick={this.handlerDefence} style={{ width: "200px" }}>
+          -1
+        </button>
       </div>
     );
   }
