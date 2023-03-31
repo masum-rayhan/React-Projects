f625788ab0ab71c8a5229ba4c3f04ea09b1e7be0
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-03-31 22:56:38 +0600

    beautify the UI

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -11,45 +11,89 @@ class Counter extends React.Component {
       count: 0,
     };
   }
-  handlerAttack() {
-    this.setState((previuosState) => ({
-      count: previuosState.count + 1,
-    }))
-  }
-  handlerDefence() {
-    this.setState((previousState) => ({
-      count: previousState.count - 1,
-    }))
-  }
+
+  handlerAttack = () => {
+    this.setState((previuosState) => {
+      let newCount = previuosState.count + Math.round(Math.random() * 10);
+      return {
+        count: newCount,
+      };
+    });
+  };
+
+  handlerDefence = () => {
+    this.setState((previuosState) => {
+      let newCount = previuosState.count - Math.round(Math.random() * 10);
+      return {
+        count: newCount,
+      };
+    });
+  };
+
+  handlerRandomPlay = () => {
+    let playMode = Math.round(Math.random());
+    if (playMode === 0) {
+      this.handlerAttack();
+    } else {
+      this.handlerDefence();
+    }
+  };
+
+  handlerReset = () => {
+    this.setState(() => {
+      return {
+        count: 0,
+      };
+    });
+  };
+
   render() {
     return (
-      <div className="row text-white">
+      <div className="row text-white text-center">
         <h1>Game Score: {this.state.count}</h1>
         <p>You win at +10 points and lose at -10 point!</p>
         <p>Last Play: </p>
         <p>Game Status: </p>
         <div className="col-6 col-md-3 offset-md-3">
-          <img style={{
-            width: "90%",
-            cursor: "pointer",
-            border: "1px solid green"
-          }} 
-          className="p-4 rounded"
-          src={attack} alt="attack lolgo" onClick={this.handlerAttack}></img>
-        </div>
-        <div className="col-6 col-md-3 offset-md-3">
-          <img style={{
-            width: "90%",
-            cursor: "pointer",
-            border: "1px solid green"
+          <img
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid green",
             }}
             className="p-4 rounded"
-            src={defense} alt="defense lolgo" onClick={this.handlerDefence}></img>
+            src={attack}
+            alt="attack lolgo"
+            onClick={this.handlerAttack}
+          ></img>
+        </div>
+        <div className="col-6 col-md-3">
+          <img
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid red",
+            }}
+            className="p-4 rounded"
+            src={defense}
+            alt="defense lolgo"
+            onClick={this.handlerDefence}
+          ></img>
         </div>
         <div className="col-12 col-md-4 offset-md-4">
-          <button className="btn btn-secondary w-100 mt-2">Random Play</button>
-          <br/>
-          <button className="btn btn-warning w-100 mt-2">Reset</button>
+          <button
+            className="btn btn-secondary w-100 mt-2"
+            onClick={this.handlerRandomPlay}
+          >
+            Random Play
+          </button>
+          <br />
+          <button
+            className="btn btn-warning w-100 mt-2"
+            onClick={this.handlerReset}
+          >
+            Reset
+          </button>
         </div>
       </div>
     );
--- countopedia/src/Header.jsx
+++ countopedia/src/Header.jsx
@@ -1,7 +1,7 @@
 import logo from "./images/react.png";
 const Header = () => {
   return (
-    <div>
+    <div className="text-center">
       <div className="pt-2 py-1 pl-2" style={{ borderBottom: "1px solid #777" }}>
         <img src={logo} style={{height: "35px", verticalAlign: "top"}} alt="react logo"></img>
         <span className="h2 pt-4 text-white-50">CountOPedia</span>
