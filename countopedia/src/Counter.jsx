2f28e6db43eb4bf3f964d2b1ce5a4789c21b6c60
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-03-31 15:20:59 +0600

    practice pervious codes

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -5,30 +5,23 @@ class Counter extends React.Component {
     this.handlerAttack = this.handlerAttack.bind(this);
     this.handlerDefence = this.handlerDefence.bind(this);
     this.state = {
-      count: 0,
-    };
+      count: 0
+    }
   }
-
-  handlerAttack() {
-    // alert("Attack");
-    this.setState({ count: this.state.count + 1});
+  handlerAttack(){
+    // alert("Attack!");
+    this.setState({count: this.state.count + 1});
   }
-
-  handlerDefence() {
-    // alert("Defend");
-    this.setState({ count: this.state.count - 1});
+  handlerDefence(){
+    // alert("Defence!");
+    this.setState({count: this.state.count - 1});
   }
-
   render() {
     return (
       <div className="row text-white">
-        <h1>Counter: {this.state.count} </h1>
-        <button onClick={this.handlerAttack} style={{ width: "200px" }}>
-          +1
-        </button>
-        <button onClick={this.handlerDefence} style={{ width: "200px" }}>
-          -1
-        </button>
+        <h1>Counter: {this.state.count}</h1>
+        <button onClick={this.handlerAttack} style={{width:"200px"}}>+1</button>
+        <button onClick={this.handlerDefence} style={{width:"200px"}}>-1</button>
       </div>
     );
   }
