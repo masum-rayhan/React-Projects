94916a516c919785970a3870637a3b0daac9d39e
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-03-31 17:52:56 +0600

    new es6 implementation

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -5,23 +5,31 @@ class Counter extends React.Component {
     this.handlerAttack = this.handlerAttack.bind(this);
     this.handlerDefence = this.handlerDefence.bind(this);
     this.state = {
-      count: 0
-    }
+      count: 0,
+    };
   }
-  handlerAttack(){
-    // alert("Attack!");
-    this.setState({count: this.state.count + 1});
+  handlerAttack() {
+    this.setState((previuosState) => ({
+      count: previuosState.count + 1,
+    }))
+    // this.setState({ count: this.state.count + 1 });
   }
-  handlerDefence(){
-    // alert("Defence!");
-    this.setState({count: this.state.count - 1});
+  handlerDefence() {
+    this.setState((previousState) => ({
+      count: previousState.count - 1,
+    }))
+    // this.setState({ count: this.state.count - 1 });
   }
   render() {
     return (
       <div className="row text-white">
-        <h1>Counter: {this.state.count}</h1>
-        <button onClick={this.handlerAttack} style={{width:"200px"}}>+1</button>
-        <button onClick={this.handlerDefence} style={{width:"200px"}}>-1</button>
+        <h1>Game Score: {this.state.count}</h1>
+        <button onClick={this.handlerAttack} style={{ width: "200px" }}>
+          +1
+        </button>
+        <button onClick={this.handlerDefence} style={{ width: "200px" }}>
+          -1
+        </button>
       </div>
     );
   }
