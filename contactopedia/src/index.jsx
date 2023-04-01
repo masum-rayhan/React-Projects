85dace8197cb11ab0ccae1d8c891908c5c1c28b7
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-04-02 03:46:20 +0600

    add necessary components

---
+++ contactopedia/src/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import Header from "./Header";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <div style={{minBlockSize: "100vh", backgroundColor: "black"}}>
+        <Header />
+    </div>
+);
