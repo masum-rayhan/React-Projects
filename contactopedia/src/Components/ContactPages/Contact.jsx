1edc4e9ce626bc224c06185249df410add5964aa
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-04-02 22:40:24 +0600

    minor

--- contactopedia/src/Components/ContactPages/Contact.jsx
+++ contactopedia/src/Components/ContactPages/Contact.jsx
@@ -11,6 +11,7 @@ const Contact = (props) => {
           style={{ width: "80%" }}
         />
       </div>
+
       <div className="col-6 col-md-5 pt-0 text-warning">
         <span className="h4">{props.contact.name}</span> <br />
         <div className="text-white-50">
@@ -18,6 +19,7 @@ const Contact = (props) => {
           {props.contact.phone}
         </div>
       </div>
+
       <div className="col-2 col-md-2 pt-md-3">
         <button
           className={`btn btn-sm m-1 ${
@@ -27,12 +29,13 @@ const Contact = (props) => {
           <i class="bi bi-star" style={{ fontSize: "1rem" }}></i>
         </button>
       </div>
+
       <div className="col-2 col-md-2 pt-md-3">
         <button className="btn btn-sm m-1 btn-outline-primary">
-            <i class="bi bi-pencil-square" style={{ fontSize: "1rem" }}></i>
+          <i class="bi bi-pencil-square" style={{ fontSize: "1rem" }}></i>
         </button>
         <button className="btn btn-sm m-1 btn-outline-danger">
-            <i class="bi bi-trash-fill" style={{ fontSize: "1rem" }}></i>
+          <i class="bi bi-trash-fill" style={{ fontSize: "1rem" }}></i>
         </button>
       </div>
     </div>
