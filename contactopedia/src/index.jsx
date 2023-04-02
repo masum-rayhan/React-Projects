bae7a46ca6ea3063815284bd79e9d6d794427300
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-04-02 15:39:28 +0600

    skelaton of components

--- contactopedia/src/index.jsx
+++ contactopedia/src/index.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
-import Header from "./Components/Layout/Header.jsx";
+import ContactIndex from "./Components/ContactPages/ContactIndex.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <div style={{minBlockSize: "100vh", backgroundColor: "black"}}>
-        <Header />
+        <ContactIndex />
     </div>
 );
